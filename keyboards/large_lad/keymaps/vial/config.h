8b4a5622ea490ba34ba5eb813a12ac7c8aca3d78
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2024-12-21 16:22:19 -0800

    large lad vial port, working well

---
+++ keyboards/large_lad/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define DEBOUNCE 15
+#define FORCE_NKRO
+#define VIAL_KEYBOARD_UID {0x4A, 0x33, 0xED, 0xF0, 0x7A, 0x68, 0xC3, 0xDD}
+#define VIAL_UNLOCK_COMBO_ROWS { 11, 11, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 12, 6, 7 }
